1971d5bb783cadc5f19b0e1433816188a7b0403d
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-03-15 23:30:12 +0300

    created projects app

--- projectmanagement/urls.py
+++ projectmanagement/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("projects/", include("projects.urls")),
 ]
 
 if settings.DEBUG:
